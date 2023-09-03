b594945a602ab0462aa9fad8ce37cb45125680af
Rosya-edwica <yunoshev.dev@gmail.com>
2023-09-03 18:35:42 +0300

    рефакторинг

--- gpt.py
+++ gpt.py
@@ -1,7 +1,6 @@
 import openai
 import os
 from dotenv import load_dotenv
-# from async_openai import OpenAI
 
 env = load_dotenv(".env")
 if not env:
@@ -11,7 +10,7 @@ if not env:
 def send_request(query: str) -> str:
     openai.api_key = os.getenv("GPT_TOKEN")
     
-    MODEL = "gpt-4"
+    MODEL = "gpt-3.5-turbo"
     response = openai.ChatCompletion.create(
         model=MODEL,
         messages=[
@@ -24,16 +23,3 @@ def send_request(query: str) -> str:
     )
     answers = [i["message"]["content"] for i in response["choices"]]
     return answers[0]
-
-
-# async def async_send_request(query: str) -> str:
-#     OpenAI.configure(
-#         api_key=os.getenv("GPT_TOKEN"),
-        
-#     )
-#     result = await OpenAI.completions.async_create(
-#         prompt=query,
-#         max_tokens=4,
-#         stream=True
-#     )
-#     print(result)
--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 from aiogram import Bot, executor, Dispatcher, types
-import os
-import gpt
-from dotenv import load_dotenv
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from datetime import datetime, timedelta
-from time import perf_counter
+from dotenv import load_dotenv
 
-env = load_dotenv(".env")
-if not env:
-    exit("Создайте файл .env")
+from datetime import datetime, timedelta
+import os
+
+import gpt
+
     
 bot = Bot(os.getenv("BOT_TOKEN"))
 dp = Dispatcher(bot)
@@ -16,19 +14,17 @@ dp = Dispatcher(bot)
 scheduler = AsyncIOScheduler()
 scheduler.start()
 
+
 @dp.message_handler(commands='start')
 async def run_bot(message: types.Message):
     await message.answer('Привет от GPT-4! Я могу ответить на все твои вопросы')
-    # logger.info(f"{message.from_user.username, message.from_user.full_name, message.from_user.id} Начал работу с ботом", )
 
 @dp.message_handler()
 async def echo(message: types.Message):
-    start_time = perf_counter()
-    
     msg = await bot.send_message(message.chat.id, "⏳ Подготовка ответа...", reply_to_message_id=message.message_id)
     gpt_answer = gpt.send_request(message.text)
+    
     date = datetime.now() + timedelta(seconds=1) 
-    end_time = round(perf_counter() - start_time)
     print(f"Message: {message.text}\nAnswer: {gpt_answer}")
     scheduler.add_job(change_message, "date", run_date=date, kwargs={"message": msg, "text": gpt_answer + f"\n\nВремя выполнения запроса: {end_time}"})
 
@@ -36,5 +32,9 @@ async def echo(message: types.Message):
 async def change_message(message: types.Message, text: str):
     await message.edit_text(text)
 
+
 if __name__ == "__main__":
+    env = load_dotenv(".env")
+    if not env:
+        exit("Создайте файл .env")
     executor.start_polling(dp, skip_updates=True)
